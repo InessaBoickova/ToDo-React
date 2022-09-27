b16ed955de22c0e54af97026490a2cb6250e6d27
Inessa <inessa.boickova@yandex.ru>
2022-09-27 10:46:42 +0300

    create done task

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './AddTask.sass'
 
 function AddTask(props){
+    let [classLict , setClassList] = useState('task');
+
+    const onDone = () =>{
+        if(classLict.includes('done')){
+            setClassList(classLict.replace('done',''))
+        }else{
+            setClassList(classLict += ' done')
+            
+        }
+        
+    }
+
     return (
-        <div className="task">
-            <div className="task_subheader">{props.task}</div>
+        <div className={classLict}>
+            <div onClick={onDone} className="task_subheader">{props.task}</div>
             <div>
                 <button  className='task_button change' onClick={props.edit}></button>
                 <button className="task_button delite" onClick={props.delite}></button>
